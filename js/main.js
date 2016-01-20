b3b711ecef7d8acb30cf05b7ae998fcae2ef5bd4
userman <userman@example.com>
2016-01-20 11:00:35 +0300

    Test button for GA added.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-/**
- * Created by youser on 1/17/16.
- */
-$(document).ready(function($){
-    fbq('track', 'ViewContent');
-});
+window.onload = function () {
+    $(document).ready(function ($) {
+        fbq('track', 'ViewContent');
+    });
+}
